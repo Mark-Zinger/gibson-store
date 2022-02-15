f1288af232fd0573424a6ccc06a095ce37296104
Mark-Zinger <lllessive@gmail.com>
2022-02-15 23:55:15 +0700

    initialize nextjs styled components bootstrap

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+// import Link from 'next/link'
+
+
+
+const Home: NextPage = () => {
+  return (
+    <div className="page">
+
+    </div>
+  )
+}
+
+export default Home
